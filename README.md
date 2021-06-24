243588fc162da6ee51bf6c735273719faebaa656
limunosekai <powerlsh0103@gmail.com>
2021-06-24 16:06:49 +0900

    Display Info Component

--- README.md
+++ README.md
@@ -4,10 +4,12 @@
 2. deploy : Netlify
 3. tools :
 
-- react 17.0.2
+- react 17.0.2 (Hook)
 - react-router-dom 5.2.0
 - react-fusioncharts 3.1.2
 - react-icons 4.2.0
 - styled-components 5.3.0
 - axios 0.21.1
 - auth0-react 1.5.0
+- github API
+- context API
--- src/components/Info.js
+++ src/components/Info.js
@@ -56,11 +56,62 @@ const Wrapper = styled.section`
   }
 `;
 
-function Info() {
-  const data = useContext(GithubContext);
+const Item = ({ icon, label, value, color }) => {
+  return (
+    <article className='item'>
+      <span className={color}>{icon}</span>
+      <div>
+        <h3>{value}</h3>
+        <p>{label}</p>
+      </div>
+    </article>
+  );
+};
 
-  console.log(data);
-  return <Wrapper></Wrapper>;
+function Info() {
+  const { githubUser } = useContext(GithubContext);
+  const { public_repos, followers, following, public_gists } = githubUser;
+
+  const items = [
+    {
+      id: 1,
+      icon: <GoRepo className='icon' />,
+      label: 'Repos',
+      value: public_repos,
+      color: 'pink',
+    },
+    {
+      id: 2,
+      icon: <FiUsers className='icon' />,
+      label: 'Followers',
+      value: followers,
+      color: 'green',
+    },
+    {
+      id: 3,
+      icon: <FiUserPlus className='icon' />,
+      label: 'Following',
+      value: following,
+      color: 'purple',
+    },
+    {
+      id: 4,
+      icon: <GoGist className='icon' />,
+      label: 'Gists',
+      value: public_gists,
+      color: 'yellow',
+    },
+  ];
+
+  return (
+    <section className='section'>
+      <Wrapper className='section-center'>
+        {items.map((item) => {
+          return <Item key={item.id} {...item}></Item>;
+        })}
+      </Wrapper>
+    </section>
+  );
 }
 
 export default Info;
